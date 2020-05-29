9259dad5c3147ef056dce3ad236a24a1952b5e5b
sohyunkim <54058041+chloesohyun@users.noreply.github.com>
2020-05-29 23:10:36 +0900

    Edited hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 /* test */
 
 int main() {
-printf("<h1>Hello, Hongik!\n\tSWE2020 class - B711025 Kim Sohyun.</h1>\n");
+printf("<h1>Hello, Hongik! SWE2020 class - B711025 Kim Sohyun</h1>\n");
 
 return 0;
 }
